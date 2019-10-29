8f84d41991fc6d4cd53b78fc19358b5f460dfe02
Suman Koirala <sumanrakoirala@gmail.com>
2019-10-28 21:41:18 -0500

    espeak addefd

--- app/admin/display_phases.rb
+++ app/admin/display_phases.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register DisplayPhase do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :active_phase
+  permit_params :active_phase
   #
   # or
   #
@@ -14,5 +14,5 @@ ActiveAdmin.register DisplayPhase do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+
 end
